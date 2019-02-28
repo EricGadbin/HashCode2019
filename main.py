71c42d0f920fb90736a4720feb03c09b388d3bd4
mathis-chaptinel <mathis.chaptinel@epitech.eu>
2019-02-28 19:45:18 +0100

    lo

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ import sys
 if len(sys.argv) < 2 or len(sys.argv) > 2:
     sys.exit(84)
 
-# try :
 slideShow = []
+means_nb_tags = 0
+nb_image = 0
 with open(sys.argv[1]) as f:
     for line in f:
         array = line.split(' ')
-        # print (array)
         if len(array) > 1:
             slideShow.append(Slide(array[0], array[2:], array[1]))
-# except:
-#     sys.exit(84)
+            means_nb_tags += int(array[1])
+        else:
+            nb_image = int(array[0])
+print  (int(means_nb_tags / nb_image))
