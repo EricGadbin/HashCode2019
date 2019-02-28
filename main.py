531cbc380a05310cca7d9431cb6941e682bcf2c0
EricGadbin <eric.gadbin@epitech.eu>
2019-02-28 20:41:55 +0100

    concatV

--- main.py
+++ main.py
@@ -17,3 +17,18 @@ with open(sys.argv[1]) as f:
             i += 1
 # except:
 #     sys.exit(84)
+slide1 = None
+slide2 = None
+for slide in pictures:
+    if (slide1 != None and slide.type == "V"):
+        slide2 = slide
+    if (slide1 == None and slide.type == "V"):
+        slide1 = slide
+    if (slide1 != None and slide2 != None):
+        slide.ConcatV(slide1, slide2)
+        pictures.pop(int(slide2.id[0]))
+        slide1 = None
+        slide2 = None
+
+for slide in pictures:
+    print (slide.id, slide.type, slide.nbTags, slide.tags)
--- slide.py
+++ slide.py
@@ -1,9 +1,16 @@
 
 class Slide():
     def __init__(self, pictureType, tags, nbTags, picId):
-        self.id = picId
+        self.id = str(picId)
         self.type = pictureType
         self.nbTags = int(nbTags)
         self.tags = tags
         self.tags[self.nbTags - 1] = self.tags[self.nbTags - 1][: len(self.tags[self.nbTags - 1]) - 1]
-        print (self.id, self.type, self.nbTags, self.tags)
+        #print (self.id, self.type, self.nbTags, self.tags)
+
+    def ConcatV(self, slide1, slide2):
+        slide1.tags += slide2.tags
+        slide1.tags = list(set(slide1.tags))
+        slide1.id += " " + slide2.id
+        slide1.type = "H"
+        slide1.nbTags = len(slide1.tags)
